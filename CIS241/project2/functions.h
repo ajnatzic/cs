a52d7e1aaab89309234a581cade347fe24f3f171
AJ Natzic <alfred.natzic@sbcglobal.net>
2019-02-18 12:45:06 -0500

    started project 2

---
+++ CIS241/project2/functions.h
@@ -0,0 +1,22 @@
+/**
+ * This is the functions header file that will define functions used in "functions.c". Most of these were pulled
+ * from the project guidelines.
+ * */
+
+// remove duplicate characters in array word and return the resulting string
+char * removeDuplicates(char word []);
+
+// search the first num characters in array charArray for character target
+// return a non-zero integer if found, otherwise, return 0
+int targetFound(char charArray[], int num, char target);
+
+// initialize the encrypt array with appropriate cipher letters according to the given key
+void initializeEncryptArray(char key[], char encrypt[]);
+
+// initialize decrypt array with appropriate substitute letters based on the encrypt array
+void initializeDecryptArray(char encrypt[], char decrypt[]);
+
+// process data from the input file and write the result to the output file
+// pass the encrypt array to parameter substitute if encryption is intended
+// pass the decrypt array to parameter substitute if decryption is intended
+void processInput(FILE * inf, FILE * outf, char substitute[]);
---
+++ CIS241/project2/project2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "functions.h"
+
+/**
+ * Main function that takes the users input and encrypts or decrpyts messages. Also checks for typos in user input
+ * */
